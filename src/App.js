dc3a1f6647b0217c6492a95c36a495084e7f8f87
AghaDev <mohamadagha2@gmail.com>
2023-09-14 16:52:22 +0300

    Updating Functional Components

--- src/App.js
+++ src/App.js
@@ -10,63 +10,25 @@ import snow from "./img/weather-icons/snow.svg";
 import storm from "./img/weather-icons/storm.svg";
 import unknown from "./img/weather-icons/unknown.svg";
 import Search from "./components/Search";
-
-
-
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
-
 import "./App.css";
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     name: "alialahmad_MohammadAlAgha"
-  //   };
-  // }
-
-  // handleInputChange = value => {
-  //   this.setState({ name: value });
-  // };
-  render() {
-    return (
-      // <div className="app">
-      //   /* <SayHi />
-      //   <SayHello color="black" name={this.state.name} />
-      //   <Search handleInput={this.handleInputChange} /> */
-      // </div>
 
 
-      // <div> alialahmad_MohammadAlAgha</div>
-        <div>
-      <Header />
-      <Main />
-      <Footer />
-      
-
-        </div>
-      
-    );
 
 
-      ;
-    }
-  }
+
+const App = () => {
+  return (
+    <div><Header />
+    <Main />
+    <Footer /></div>
+
+  )
+}
 
 export default App;
-
-{/* <img src={mc}/>
-<p>09:00</p>
-<img src={mc}/>
-<p>12:00</p>
-<img src={mc}/>
-<p>15:00</p>
-<img src={mc}/>
-<p>18:00</p>
-<img src={mc}/>
-<p>21:00</p>
-<img src={mc}/> */}
